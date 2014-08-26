6ee0e0ad925f4710dd7596fd68555c468b16302f
calam1 <chrislam67@gmail.com>
2014-08-26 17:30:16 -0500

    initial commit

---
+++ src/study_clojure/core.clj
@@ -0,0 +1,17 @@
+(ns study-clojure.core
+  (:require
+   [clojure.string :as s]
+   [clojure.math.combinatorics :as combo]))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
+
+(defn reverseStringUsingStringReverseMethod
+  [arg]
+  (s/reverse arg))
+
+(defn reverseStringBuiltInReverseMethod
+  [arg]
+  (apply str (reverse arg)))
