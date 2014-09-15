24e063e1f7a5a5633883ed40d48b4f212e200d8e
calam1 <chrislam67@gmail.com>
2014-09-15 17:09:49 -0500

    added a lazy seq that will not overflow

--- src/study_clojure/fibonacci.clj
+++ src/study_clojure/fibonacci.clj
@@ -13,4 +13,16 @@
 (defn fibonacci_naive [num]
   (if (= num 0) 0
      (if (= num 1) 1
-      (+ (fibonacci_naive (- num 2)) (fibonacci_naive (- num 1))))))
+         (+ (fibonacci_naive (- num 2)) (fibonacci_naive (- num 1))))))
+
+;; lazy seq will infinitely generate fibonacci sequence - will not
+;; overflow cause of the quote after the addition sign - converts it
+;; to BigInteger  if needed
+(def fibonacci-seq
+  ((fn recursiveFibonacci [a b]
+     (lazy-seq (cons a (recursiveFibonacci b (+' a b)))))
+   0 1))
+
+;; uses the above seq and cons the results - will not overflow
+(defn lazy-fib [number]
+   (take number fibonacci-seq))
